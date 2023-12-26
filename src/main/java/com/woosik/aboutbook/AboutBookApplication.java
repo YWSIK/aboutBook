1c46470d34aab2b0929d9f45424cbb23db64ab2a
YWSIK <yuusik818@gmail.com>
2023-12-27 03:58:21 +0900

    프로젝트 추가

---
+++ src/main/java/com/woosik/aboutbook/AboutBookApplication.java
@@ -0,0 +1,13 @@
+package com.woosik.aboutbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woosik/aboutbook/AboutBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woosik.aboutbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
